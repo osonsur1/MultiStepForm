d77bd21b32b4da5e6e5699a636ec28a6c851fd0f
Omkar Sonsurkar <osonsurkar@radiantlogic.com>
2019-09-17 15:45:04 -0700

    Add menu bar

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
+    render() {
+        const { values, handleChange } = this.props;
+        
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter User Details" />
+                    <TextField 
+                        hintText="Enter Your First Name"
+                        floatingLabelText="First Name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                    <br/>
+                    <TextField 
+                        hintText="Enter Your First Name"
+                        floatingLabelText="First Name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default FormUserDetails
